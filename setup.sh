52f48ea3b1056011fb4e200f26a565ca659aa826
hez0_ <103249837+hez01@users.noreply.github.com>
2023-01-25 07:25:02 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pkg update
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Debian/debian.sh && bash debian.sh to install proot'ed Debian (https://github.com/EXALAB/AnLinux-App)
+su
+export PATH=$PATH:/data/data/com.termux/files/usr/bin/
+./start-debian.sh
+apt update
+wget https://raw.githubusercontent.com/hez01/pihole-termux/main/install.sh
+chmod u+x install.sh
+./install.sh;
+./install.sh
